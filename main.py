13a3233e9dd3b9a196091adda42ee5362675563b
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2025-02-16 02:20:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import requests
 from pprint import pprint
-from graph_agents import app
+from graph_agents import compile_graph
+import langsmith 
 from dotenv import load_dotenv
 load_dotenv()
 
-# LangSmith Tracing
+question = "What was the last ufc event and who won?"
 
-def main():
+def main(app, question):
     inputs = {
-        "question": "What was the last ufc event and who won?"
+        "question": question
     }
     for output in app.stream(inputs):
         for key, value in output.items():
@@ -17,7 +19,19 @@ def main():
         pprint("\n---\n")
 
     # Final generation
-    pprint(value["generation"]) # generate: {question, generation, documents}
+    return value["generation"] # generate: {question, generation, documents}
 
 if __name__ == "__main__":
-    main()
+    app = compile_graph()
+    response = main(app, question)
+    print(response)
+
+    url = "http://127.0.0.1:5000/ask"
+    data = {"question": question}
+    response = requests.post(url, json=data)
+    print(response.json())
+
+"""
+Request: 
+curl -X POST http://127.0.0.1:5000/ask -H "Content-Type: application/json" -d '{"question": "What was the last UFC event and who won?"}'
+"""
