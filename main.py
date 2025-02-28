a8946c0089ea6b98b4e94cfc42252d9964949d3d
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2025-02-28 13:35:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import requests
 from pprint import pprint
 from graph_agents import compile_graph
+from vector_db_ops import VectorDB
 import langsmith 
 from dotenv import load_dotenv
 load_dotenv()
-
-question = "What was the last ufc event and who won?"
 
 def main(app, question):
     inputs = {
@@ -22,10 +21,23 @@ def main(app, question):
     return value["generation"] # generate: {question, generation, documents}
 
 if __name__ == "__main__":
+    question = "How do black holes work?"
+    vectordb = VectorDB()
+    
+    docs = [] # List of file paths and urls 
+    vectordb.store_vectordb(docs)
     app = compile_graph()
     response = main(app, question)
     print(response)
 
+    # Upload documents 
+    url = "http://127.0.0.1:5000/upload"
+    files = [("files", open("files/test.pdf", "rb")), ("files", open("files/test.txt", "rb"))]
+    data = {"urls": ["https://example.com/article1", "https://example.com/article2"]}
+    response = requests.post(url, files=files, data=data)
+    print(response.json())
+
+    # Ask question 
     url = "http://127.0.0.1:5000/ask"
     data = {"question": question}
     response = requests.post(url, json=data)
@@ -34,4 +46,7 @@ if __name__ == "__main__":
 """
 Request: 
 curl -X POST http://127.0.0.1:5000/ask -H "Content-Type: application/json" -d '{"question": "What was the last UFC event and who won?"}'
+npm start - start react app
+python api.py - start flask server 
+langgraph studio - langgraph dev (config from langgraph.json)
 """
